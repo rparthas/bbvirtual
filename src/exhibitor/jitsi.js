92efd1aeb45e3748cdf2f76abddd03f70b5e64d6
Rajagopal Parthasarathi <rajagopal.sarathi@gmail.com>
2021-02-01 10:31:24 +0530

    Initial commit of repo

---
+++ src/exhibitor/jitsi.js
@@ -0,0 +1,98 @@
+var s = document.getElementsByTagName('script')[0];
+
+var jitsi = document.createElement('script');
+jitsi.type = 'text/javascript'; 
+jitsi.async = true; 
+jitsi.defer = true; 
+jitsi.src = 'https://meet.jit.si/external_api.js';
+// jitsi.src = 'https://localhost:8443/libs/lib-jitsi-meet.min.js'; 
+s.parentNode.insertBefore(jitsi, s);
+
+
+var scanner = document.createElement('script');
+scanner.type = 'text/javascript'; 
+scanner.async = true; 
+scanner.defer = true; 
+scanner.src = 'https://rawgit.com/schmich/instascan-builds/master/instascan.min.js'; 
+s.parentNode.insertBefore(scanner, s);
+
+
+var api;
+var participants = [];
+
+function displayParticipants(){
+    const activeParticipants = participants.filter(participant =>participant.status == 'Active');
+    const inActiveParticipants = participants.filter(participant =>participant.status == 'InActive');
+    document.getElementById('activeCnt').innerHTML = activeParticipants.length;
+    document.getElementById('pastCnt').innerHTML = inActiveParticipants.length;
+    $('#activeParticipants').html('<ul id="activeParticipantsList" class="list-group"></ul>');
+    activeParticipants.forEach(participant => {$('#activeParticipantsList').append(`<li class="list-group-item">${participant.displayName}</li>`)});
+    $('#inactiveParticipants').html('<ul id="inactiveParticipantsList" class="list-group"></ul>');
+    inActiveParticipants.forEach(participant => {$('#inactiveParticipantsList').append(`<li class="list-group-item">${participant.displayName}</li>`)});
+}
+
+function init(){
+    const domain = 'meet.jit.si';
+    const name = '1ff7c918-50de-49c0-8626-9a4894c23b8b Oreilly Exhibitor';
+    const options = {
+        roomName:name,
+        width: 500,
+        height: 500,
+        parentNode: document.querySelector('#video'),
+        userInfo: {
+            displayName: 'Oreilly Exhibitor'
+        },
+        configOverwrite : {enableWelcomePage : false}
+    };
+    api = new JitsiMeetExternalAPI(domain, options);
+    api.addEventListener('endpointTextMessageReceived',(event)=>{
+        document.getElementById('scannedText').innerHTML = "<div> Scanned this: "+event.data.eventData.text+"</div>";
+    });
+    api.addEventListener('participantJoined',(event)=>{
+        var participant = api.getParticipantsInfo().filter(participant =>participant.participantId === event.id)[0];
+        participant.status = 'Active';
+        participants.push(participant);
+        displayParticipants();
+    });
+    api.addEventListener('participantLeft',(event)=>{
+        participants.filter(participant =>participant.participantId == event.id)[0].status ='InActive';  
+        displayParticipants();
+    });
+    api.addEventListener('videoConferenceLeft',(_)=>{
+        api.dispose();
+        $('#startBtn').show();
+    });
+    api.addEventListener('emailChange',(event)=>{
+        console.log("### Email change###",event);
+    });
+}
+
+
+function scan(){
+    let scanner = new Instascan.Scanner({
+        video: document.getElementById('preview'),
+        scanPeriod: 5
+    });
+
+    scanner.addListener('scan', function (content) {
+        console.log(content);
+        api.executeCommand("sendEndpointTextMessage", "", content);
+    });
+    Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 0) {
+            scanner.start(cameras[0]);
+        } else {
+            console.error('No cameras found.');
+        }
+    }).catch(function (e) {
+        console.error(e);
+    });
+}
+
+function start() {
+    init();
+    scan();
+    $('#startBtn').hide();
+}
+
+
---
+++ src/visitor/jitsi.js
@@ -0,0 +1,62 @@
+var s = document.getElementsByTagName('script')[0];
+
+var jitsi = document.createElement('script');
+jitsi.type = 'text/javascript'; 
+jitsi.async = true; 
+jitsi.defer = true; 
+jitsi.src = 'https://meet.jit.si/external_api.js';
+// jitsi.src = 'https://localhost:8443/libs/lib-jitsi-meet.min.js'; 
+s.parentNode.insertBefore(jitsi, s);
+
+
+var scanner = document.createElement('script');
+scanner.type = 'text/javascript'; 
+scanner.async = true; 
+scanner.defer = true; 
+scanner.src = 'https://rawgit.com/schmich/instascan-builds/master/instascan.min.js'; 
+s.parentNode.insertBefore(scanner, s);
+
+
+var api;
+
+function init(){
+    const domain = 'meet.jit.si';
+    const name = "1ff7c918-50de-49c0-8626-9a4894c23b8b Oreilly Exhibitor";
+    const options = {
+        roomName:name,
+        width: 500,
+        height: 500,
+        parentNode: document.querySelector('#video'),
+        configOverwrite: { startWithAudioMuted: true, enableWelcomePage : false },
+        userInfo: {
+            displayName: $("#name").val(),
+            avatarURL: $("#email").val()
+        }
+    };
+    api = new JitsiMeetExternalAPI(domain, options);
+    api.executeCommand('email', $("#email").val());
+    api.addEventListener('endpointTextMessageReceived',(event)=>{
+        $("#scannedText").html(`<div> Scanned this: ${event.data.eventData.text}</div>`);
+    });
+    api.addEventListener('videoConferenceLeft',(_)=>{
+        api.dispose();
+        $('#join').show();
+    });
+}
+
+function start() {
+    init();
+    $('#join').hide();
+}
+
+function validate(){
+    var name = $("#name").val();
+    var email = $("#email").val();
+    if (name && name.length >0 && email && email.length>4){
+        $("#join").prop('disabled', false);
+    }else{
+        $("#join").prop('disabled', true);
+    }
+}
+
+
